70d94fcf82b8f952184b4d98a74452f01b7c1a8d
RainRat <rainrat78@yahoo.ca>
2026-03-05 14:57:44 -0800

    Apply SEE-unreliable guard in GOOD_CAPTURE staging

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -365,7 +365,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-69 * cur->value / 1024 - 500 * (pos.captures_to_hand() && pos.gives_check(*cur))))?
+                       return (pos.see_pruning_unreliable() || pos.see_ge(*cur, Value(-69 * cur->value / 1024 - 500 * (pos.captures_to_hand() && pos.gives_check(*cur)))))?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
