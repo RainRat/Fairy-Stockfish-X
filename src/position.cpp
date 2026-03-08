24fd19e32e6931bc93f403f2618194a4c1f072ee
RainRat <rainrat78@yahoo.ca>
2026-03-08 01:47:22 -0800

    Avoid extra check-info work in kingless positions

--- src/position.cpp
+++ src/position.cpp
@@ -1029,21 +1029,23 @@ void Position::set_check_info(StateInfo* si) const {
 
   // For unused piece types, the check squares are left uninitialized
   si->nonSlidingRiders = 0;
-  for (PieceSet ps = piece_types(); ps;)
-  {
-      PieceType pt = pop_lsb(ps);
-      PieceType movePt = pt == KING ? king_type() : pt;
-      if (ksq == SQ_NONE)
-          si->checkSquares[pt] = Bitboard(0);
-      else if (AttackRiderTypes[movePt] & ASYMMETRICAL_RIDERS)
-          // For asymmetrical riders, use true retro paths from the king square.
-          si->checkSquares[pt] = retro_asymmetric_check_squares(sideToMove, movePt, ksq, pieces());
-      else
-          si->checkSquares[pt] = attacks_bb(~sideToMove, movePt, ksq, pieces());
-      // Collect special piece types that require slower check and evasion detection
-      if (AttackRiderTypes[movePt] & NON_SLIDING_RIDERS)
-          si->nonSlidingRiders |= pieces(pt);
-  }
+  if (ksq == SQ_NONE)
+      for (PieceSet ps = piece_types(); ps;)
+          si->checkSquares[pop_lsb(ps)] = Bitboard(0);
+  else
+      for (PieceSet ps = piece_types(); ps;)
+      {
+          PieceType pt = pop_lsb(ps);
+          PieceType movePt = pt == KING ? king_type() : pt;
+          if (AttackRiderTypes[movePt] & ASYMMETRICAL_RIDERS)
+              // For asymmetrical riders, use true retro paths from the king square.
+              si->checkSquares[pt] = retro_asymmetric_check_squares(sideToMove, movePt, ksq, pieces());
+          else
+              si->checkSquares[pt] = attacks_bb(~sideToMove, movePt, ksq, pieces());
+          // Collect special piece types that require slower check and evasion detection
+          if (AttackRiderTypes[movePt] & NON_SLIDING_RIDERS)
+              si->nonSlidingRiders |= pieces(pt);
+      }
   si->shak = si->checkersBB & (byTypeBB[KNIGHT] | byTypeBB[ROOK] | byTypeBB[BERS]);
   si->bikjang = var->bikjangRule && ksq != SQ_NONE ? bool(attacks_bb(sideToMove, ROOK, ksq, pieces()) & pieces(sideToMove, KING)) : false;
   si->chased = var->chasingRule ? chased() : Bitboard(0);
