5440f551513ceb4281634dd8d1da73a542cd7dab
RainRat <rainrat78@yahoo.ca>
2026-04-08 00:15:03 -0700

    Make hopper immobility regression self-contained

--- tests/immobility-illegal-hoppers.sh
+++ tests/immobility-illegal-hoppers.sh
@@ -7,14 +7,19 @@ tmp_ini=$(mktemp)
 trap 'rm -f "$tmp_ini"' EXIT
 
 cat > "$tmp_ini" <<'INI'
-[immobility-illegal-hopper-test:shogi]
+[immobility-illegal-hopper-test:chess]
+maxFile = i
+maxRank = 9
+pieceDrops = true
+immobilityIllegal = true
+king = k:W
 customPiece1 = m:fpR
-customPiece2 = j:fC
-promotedPieceType = p:g m:g j:g s:g b:h r:d
-startFen = 2sgkgs2/1r5b1/p1ppppp1p/1p5p1/9/1P5P1/P1PPPPP1P/1B5R1/2SGKGS2[mmMMjjJJ]
+customPiece2 = g:W
+promotedPieceType = m:g
+startFen = 9/9/9/9/9/9/9/9/4K4[M]
 INI
 
-out=$(printf 'uci\nsetoption name VariantPath value %s\nsetoption name UCI_Variant value immobility-illegal-hopper-test\nposition fen 9/9/9/9/9/9/9/9/4K4[MJJjjmm] w - - 0 1\ngo perft 1\nquit\n' "$tmp_ini" | ./stockfish)
+out=$(printf 'uci\nsetoption name VariantPath value %s\nsetoption name UCI_Variant value immobility-illegal-hopper-test\nposition fen 9/9/9/9/9/9/9/9/4K4[M] w - - 0 1\ngo perft 1\nquit\n' "$tmp_ini" | ./stockfish)
 
 grep -q '^M@a7:' <<<"$out"
 grep -q '^M@e7:' <<<"$out"
