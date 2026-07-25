fe9a9f5eebc82b39d676dbddb5b2e2bd0beaa53d
RainRat <rainrat78@yahoo.ca>
2026-07-25 12:56:42 -0700

    Allow edge insertion generation on occupied edge squares for pousse edge pushes

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -396,7 +396,7 @@ namespace {
     if (!insertTypes)
         return moveList;
 
-    Bitboard entries = pos.edge_insert_region(Us) & pos.board_bb();
+    Bitboard entries = pos.edge_insert_region(Us);
     if (!entries)
         return moveList;
 
