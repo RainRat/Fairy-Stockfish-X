fbd04497a0698eed4303439ea220a92286458bd8
RainRat <rainrat78@yahoo.ca>
2026-08-15 07:47:14 -0700

    Harden Arimaa setup and turn replay

--- src/search.cpp
+++ src/search.cpp
@@ -247,7 +247,7 @@ void MainThread::search() {
   }
 
 #ifdef ENABLE_ARIMAA
-  if (rootPos.variant()->arimaa && rootPos.compound_turn_active())
+  if (rootPos.variant()->arimaa)
   {
       Time.init(rootPos, Limits, rootPos.side_to_move(), rootPos.game_ply());
       callsCnt = 1;
@@ -422,7 +422,7 @@ void MainThread::search() {
 
 void Thread::search() {
 #ifdef ENABLE_ARIMAA
-  if (rootPos.variant()->arimaa && rootPos.compound_turn_active())
+  if (rootPos.variant()->arimaa)
   {
       search_arimaa(*this);
       return;
